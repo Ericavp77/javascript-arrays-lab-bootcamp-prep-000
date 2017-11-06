5f3f3c788f5f218202dd0786452451b44a5d8c8e
Erica Pitts <ericavp77@gmail.com>
2017-11-06 18:24:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendKitten(name) {
-  appendKitten(name)
-    return appendKitten
+  kittens.push()
+    return kittens
       }
